854a79818537a71be34f5f79a33115a7452861fc
Jesioi <songjesi02@gmail.com>
2026-06-08 23:05:33 -0700

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] match = new int[26];
+        for (char c : s.toCharArray()) {
+            match[c - 'a']++;
+        }
+
+        for (char c : t.toCharArray()) {
+            match[c - 'a']--;
+        }
+
+        for (int i : match) {
+            if (i != 0) return false;
+        }
+        return true;
+    }
+}
+//approach 1:
+//array mapping number of char appeared
+//deduct for second string
+//if numbers are greater than 0 return false
+
+//O(n + m)
+//O(1) : since we have at most 26 different character !!!!
