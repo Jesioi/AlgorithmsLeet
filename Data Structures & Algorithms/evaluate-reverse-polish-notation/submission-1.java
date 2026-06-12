2b59cc759e08a559554259eaa8e3c4d81c6c262c
Jesioi <songjesi02@gmail.com>
2026-06-12 13:04:08 -0700

    Add: evaluate-reverse-polish-notation - submission-1

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-1.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (String c : tokens) {
+            if (c.equals("+")) {
+                stack.push(stack.pop() + stack.pop());
+            } else if (c.equals("-")) {
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.push(b - a);
+            } else if (c.equals("*")) {
+                stack.push(stack.pop() * stack.pop());
+            } else if (c.equals("/")) {
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.push(b / a);
+            } else {
+                stack.push(Integer.parseInt(c));
+            }
+        }
+        return stack.pop();
+    }
+}
+//brute force
+//iterate to find operator, calculate 2 string ahead. 
+
+
+//use stack to store val, pop when arithmetic expression appear
+// O(n)
+// O(n)
